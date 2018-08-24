7a807aa2b84a2b5292aa1a88e9c881e35a395b5e
darecophoenixx <tmdnro.phoenixx@gmail.com>
2018-08-25 08:09:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='wordroid.sblo.jp',
     authoer='Norio Tamada',
+    url='https://github.com/darecophoenixx/wordroid.sblo.jp',
     version='0.00.01',
     package_dir={'feature_eng': 'lib/feature_eng', 'keras_ex.GaussianKernel': 'lib/keras_ex/GaussianKernel',},
     packages=['feature_eng', 'keras_ex.GaussianKernel',],
