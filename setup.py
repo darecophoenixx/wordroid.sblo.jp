771556586137c45a5f2483937e37954afbd833a9
darecophoenixx <tmdnro.phoenixx@gmail.com>
2018-08-25 09:07:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     authoer='Norio Tamada',
     url='https://github.com/darecophoenixx/wordroid.sblo.jp',
     version='0.00.01',
-    package_dir={'feature_eng': 'lib/feature_eng', 'keras_ex.GaussianKernel': 'lib/keras_ex/GaussianKernel',},
-    packages=['feature_eng', 'keras_ex.GaussianKernel',],
+    package_dir={'feature_eng': 'lib/feature_eng', 'keras_ex.gkernel': 'lib/keras_ex/gkernel',},
+    packages=['feature_eng', 'keras_ex.gkernel',],
 )
 
