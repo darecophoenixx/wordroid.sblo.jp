5863c4beec1d1b91cd3fddb1dfb08c5ad10df991
admin <admin@lmi740bn2.darecosoho>
2021-05-09 15:21:31 +0900

    sksom_keras: change qd calc position

--- lib/som/keras.py
+++ lib/som/keras.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from keras import initializers, constraints
 from keras.layers import Layer
+from keras.layers import Lambda
 from keras.layers import Input
 from keras import backend as K
 from keras.models import Model, Sequential
@@ -16,6 +17,7 @@ from keras.optimizers import RMSprop, Adam, SGD
 class SOM(Layer):
     
     def __init__(self, map_shape, lm_init,
+                 qd,
                  r=1.0,
                  kernel_initializer='glorot_uniform',
                  kernel_constraint=None,
@@ -34,6 +36,7 @@ class SOM(Layer):
         self.lm_init = lm_init
         super(SOM, self).__init__(**kwargs)
         self.map_shape = map_shape
+        self.qd = qd
         self.output_dim = lm_init.shape
         self.num_feature = lm_init.shape[1]
         self.kernel_initializer = initializers.get(kernel_initializer)
@@ -45,9 +48,8 @@ class SOM(Layer):
         self._calc_qd()
 
     def _calc_qd(self):
-        qd = [np.array((ii,jj)) for ii in range(self.map_shape[0]) for jj in range(self.map_shape[1])]
-        qd = np.array([np.square(p1 - p2).sum() for p1 in qd for p2 in qd]).reshape(np.array(self.map_shape).prod(), np.array(self.map_shape).prod())
-        self.qd = K.cast_to_floatx(np.exp(-self.gamma * qd))
+        qd = self.qd
+        self.qd_calced = K.cast_to_floatx(np.exp(-self.gamma * qd))
     
     def compute_output_shape(self, input_shape):
         return (input_shape[0],) +  self.output_dim
@@ -61,10 +63,6 @@ class SOM(Layer):
                                          initializer=self.kernel_initializer,
                                          constraint=self.kernel_constraint)
         #print('self.landmarks.shape: ', self.landmarks.shape)
-#         self.gamma = self.add_weight(name='gamma',
-#                                      shape=(1,),
-#                                      trainable=False)
-#         print('self.gamma.shape: ', self.gamma.shape)
         super(SOM, self).build(input_shape)  # Be sure to call this somewhere!
     
     def call(self, x, training=None):
@@ -73,7 +71,7 @@ class SOM(Layer):
     def calc_delta(self, x, landmarks, gamma):
         '''
         landmarks : shape = (1200, num_feature)
-        qd : shape = (1200, 1200)
+        qd_calced : shape = (1200, 1200)
         x : shape = (None, num_feature)
         '''
         #print('landmarks.shape', landmarks.shape)
@@ -93,12 +91,12 @@ class SOM(Layer):
         #print('d: ', K.int_shape(d))
         min_idx = K.argmin(d) # (None,)
         #print('min_idx: ', K.int_shape(min_idx))
-        h = K.gather(self.qd, min_idx) # (None, 1200)
+        h = K.gather(self.qd_calced, min_idx) # (None, 1200)
         #print('h: ', K.int_shape(h))
         h = K.repeat(h, self.num_feature) # (None, 1200)
         h = K.permute_dimensions(h, (0,2,1))
         #print('h: ', K.int_shape(h))
-        x_expand = K.repeat(x, self.output_dim[0])
+        x_expand = K.repeat(x, self.output_dim[0]) # (None, 1200, num_feature)
         #print('x_expand: ', K.int_shape(x_expand))
         delta0 = h * (x_expand - landmarks) # (None, 1200, num_feature) - (1200, num_feature) -> (None, 1200, num_feature)
         #print('delta0: ', K.int_shape(delta0))
@@ -110,10 +108,153 @@ class SOM(Layer):
             'map_shape': self.map_shape,
             'lm_init': self.lm_init,
             'r': self.r,
+            'qd': self.qd,
             'kernel_initializer': initializers.serialize(self.kernel_initializer),
             'kernel_constraint': constraints.serialize(self.kernel_constraint),
         }
         base_config = super(SOM, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+
+class CalcDistance(Layer):
+    
+    def __init__(self, lm_init,
+                 kernel_initializer='glorot_uniform',
+                 kernel_constraint=None,
+                 **kwargs):
+        '''
+        map_shape:
+            shape of som_map
+            typicaly (40, 50) or (3, 4) etc.
+        lm_init:
+            initial landmarks
+            shape = (2000, num_feature) or (12, num_feature)
+        '''
+        kwargs['weights'] = [lm_init]
+        self.lm_init = lm_init
+        super(CalcDistance, self).__init__(**kwargs)
+        self.output_dim = lm_init.shape[:1]
+        self.num_feature = lm_init.shape[1]
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0],) +  self.output_dim
+    
+    def build(self, input_shape):
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+        
+        self.landmarks = self.add_weight(name='landmarks',
+                                         shape=self.lm_init.shape,
+                                         initializer=self.kernel_initializer,
+                                         constraint=self.kernel_constraint)
+        super(CalcDistance, self).build(input_shape)  # Be sure to call this somewhere!
+    
+    def call(self, x, training=None):
+        return self.calc_d2(x, self.landmarks)
+    
+    def calc_d2(self, x, landmarks):
+        '''
+        landmarks : shape = (1200, num_feature)
+        qd : shape = (1200, 1200)
+        x : shape = (None, num_feature)
+        '''
+        print('landmarks.shape', landmarks.shape)
+        
+        xlm = K.dot(x, K.transpose(landmarks)) # (None, num_feature) . (num_feature, 1200) -> (None, 1200)
+        #print('xlm: ', K.int_shape(xlm))
+        x2 = K.sum(K.square(x), axis=1) # (None,)
+        #print('x2: ', K.int_shape(x2))
+        x2 = K.reshape(x2, (-1,1)) # (None, 1)
+        #print('x2: ', K.int_shape(x2))
+        x2 = K.repeat_elements(x2, self.output_dim[0], axis=1) # (None, 1200)
+        #print('x2: ', K.int_shape(x2))
+        lm2 = K.sum(K.square(landmarks), axis=1) # (1200,)
+        #print('lm2: ', K.int_shape(lm2))
+        
+        d2 = x2 + lm2 - 2*xlm # (None, 1200)
+        return d2
+    
+    def get_config(self):
+        config = {
+            'lm_init': self.lm_init,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+        }
+        base_config = super(CalcDistance, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+
+class CalcDelta(Layer):
+    
+    def __init__(self, num_feature, qd,
+                 r=1.0,
+                 kernel_initializer=None,
+                 kernel_constraint=None,
+                 **kwargs):
+        '''
+        map_shape:
+            shape of som_map
+            typicaly (40, 50) or (3, 4) etc.
+        r:
+            gamma = 1 / (2 * r**2)
+        '''
+        super(CalcDelta, self).__init__(**kwargs)
+        self.qd = qd
+        self.map_shape = map_shape
+        self.output_dim = (qd.shape[0], num_feature)
+        self.num_feature = num_feature
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+
+        # kernel parameter
+        self.r = r
+        self.gamma = 1 / (2 * r**2)
+        
+        self.qd_calced = np.exp(-gamma * self.qd)
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0],) +  self.output_dim
+    
+    def build(self, input_shape):
+        assert len(input_shape) == 2
+        input_dim = input_shape[1]
+        
+        super(CalcDelta, self).build(input_shape)  # Be sure to call this somewhere!
+    
+    def call(self, x, training=None):
+        return self.calc_delta(x)
+    
+    def calc_delta(self, X):
+        '''
+        d2 : distance^2 shape = (None, 1200)
+        qd : shape = (1200, 1200)
+        x : shape = (None, num_feature)
+        '''
+        x, d2 = X
+        min_idx = K.argmin(d2) # (None,)
+        #print('min_idx: ', K.int_shape(min_idx))
+        h = K.gather(self.qd_calced, min_idx) # (None, 1200)
+        #print('h: ', K.int_shape(h))
+        h = K.repeat(h, self.num_feature) # (None, 1200)
+        h = K.permute_dimensions(h, (0,2,1))
+        #print('h: ', K.int_shape(h))
+        x_expand = K.repeat(x, self.output_dim[0]) # (None, 1200, num_feature)
+        #print('x_expand: ', K.int_shape(x_expand))
+        delta0 = h * (x_expand - landmarks) # (None, 1200, num_feature) - (1200, num_feature) -> (None, 1200, num_feature)
+        #print('delta0: ', K.int_shape(delta0))
+        #ret = K.exp(-gamma * d)
+        return delta0
+    
+    def get_config(self):
+        config = {
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+        }
+        base_config = super(CalcDelta, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
 
@@ -153,6 +294,13 @@ class sksom_keras(object):
         self.landmarks_ = self.init_K.copy()
         self.kmeans = self._kmeans()
         self.labels_ = self.kmeans.labels_
+        
+        self._calc_qd()
+    
+    def _calc_qd(self):
+        qd = [np.array((ii,jj)) for ii in range(self.kshape[0]) for jj in range(self.kshape[1])]
+        qd = np.array([np.square(p1 - p2).sum() for p1 in qd for p2 in qd]).reshape(np.array(self.kshape).prod(), np.array(self.kshape).prod())
+        self.qd = qd
     
     def _kmeans(self):
         kmeans = KMeans(n_clusters=self.kshape[0]*self.kshape[1], n_init=1, max_iter=1)
@@ -163,7 +311,7 @@ class sksom_keras(object):
     
     def _make_keras_model(self, r, LM):
         inp = Input(shape=(self.init_K.shape[1],), name='inp')
-        l_som = SOM(map_shape=self.kshape, lm_init=LM, r=r, name='som')
+        l_som = SOM(map_shape=self.kshape, lm_init=LM, r=r, qd=self.qd, name='som')
         oup = l_som(inp)
 
         model = Model(inp, oup, name='model')
@@ -202,7 +350,7 @@ class sksom_keras(object):
         num_feature = self.init_K.shape[1]
         LM = self.landmarks_
         for i_epochs, i_r in sche:
-            print('===== r: ', i_r, ' / epochs: ', i_epochs)
+            print('=====> r: ', i_r, ' / epochs: ', i_epochs)
             self._make_keras_model(i_r, LM)
             self.model.compile(loss=loss, optimizer=optimizer)
             res = self.model.fit(x, np.zeros((x.shape[0],1,num_feature)),
@@ -225,6 +373,67 @@ class sksom_keras(object):
             d = np.square(X - self.landmarks_[ii]).sum(axis=1)
             p_list.append(np.exp(-self.gamma * d))
         return np.vstack(p_list).T
+
+
+
+class sksom_keras2(sksom_keras):
+    
+    def __init__(self, kshape, init_K,
+                 form=None,
+                 rand_stat=0,
+                 r=None, epochs=500,
+                 early_stopping=(5, 1.0e-7),
+                 batch_size=1024,
+                 loss='mae', optimizer=SGD(learning_rate=100.0),
+                 verbose=0):
+        super(sksom_keras2, self).__init__(
+            kshape=kshape, init_K=init_K, form=form, rand_stat=rand_stat,
+            r=r,
+            epochs=epochs, early_stopping=early_stopping, batch_size=batch_size,
+            verbose=verbose,
+            loss=loss, optimizer=optimizer
+            )
+        self.num_feature = self.init_K.shape[1]
+    
+    def _make_keras_model(self, r, LM):
+        inp = Input(shape=(self.init_K.shape[1],), name='inp')
+        l_som = SOM(map_shape=self.kshape, lm_init=LM, r=r, name='som')
+        oup = l_som(inp)
+        model = Model(inp, oup, name='model')
+        
+        calc_d = CalcDistance(lm_init=LM, name='calc_d')
+        oup_d = calc_d(inp)
+        oup_min_d = Lambda(lambda d: K.min(d, axis=1), name='min_d')(oup_d)
+        model_min_d = Model(inp, oup_min_d, name='model_min_d')
+        
+#        def calc_delta(X):
+#            x, d2 = X
+#            min_idx = K.argmin(d2)
+#            qd_selected = K.gather(qd_calced, min_idx)
+#            h = K.repeat(qd_selected, self.num_feature)
+#            h = K.permute_dimensions(h, (0,2,1))
+#            x_expand = K.repeat(x, self.init_K.shape[0])
+#            delta = h * (x_expand - LM)
+#            return delta
+#        delta = Lambda(calc_delta, name='calc_delta')([inp, oup_d])
+#        oup_argmin = Lambda(lambda d: K.argmin(d), name='argmin')(oup_d)
+#        qd_selected = Lambda(lambda x: K.gather(qd_calced, x), name='qd_selected')(oup_argmin) # (None, 1200)
+#        #print('h: ', K.int_shape(h))
+#        h = Lambda(lambda x: K.repeat(x, self.num_feature), name='qd_selected_2')(qd_selected) # (None, 1200)
+#        h = Lambda(lambda x: K.permute_dimensions(x, (0,2,1)), name='h')(h)
+#        #print('h: ', K.int_shape(h))
+#        x_expand = Lambda(lambda x: K.repeat(x, self.init_K.shape[0]), name='x_expand')(inp)
+#        #print('x_expand: ', K.int_shape(x_expand))
+#        delta = Lambda(lambda x: x[0] * (x[1] - LM), name='calc_delta')([h, x_expand])
+        
+        
+        self.model = model
+        self.models = {
+            'model': model,
+            'model_min_d': model_min_d,
+        }
+        return model
+    
 
 
 
