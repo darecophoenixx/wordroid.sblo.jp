5d174ef014f33bc6678f6e4e0e601e467955d708
admin <admin@lmi740bn2.darecosoho>
2021-09-20 14:18:02 +0900

    add M01F_li

--- lib/feature_eng/m01f.py
+++ lib/feature_eng/m01f.py
@@ -175,18 +175,7 @@ class M01F(object):
         return self.mclust_row_res
     
     def plot_mclust(self, res, figsize=(15, 15), lw=2):
-        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=figsize)
-        
-        for ii, k in enumerate(cov_type_list):
-            i, j = divmod(ii, 2)
-            m = res[k].mean(axis=1)
-            std = np.std(res[k], axis=1)
-            axes[i,j].plot(self.g_range, m, label=k, marker='o')
-            axes[i,j].fill_between(self.g_range, m - std,
-                             m + std, alpha=0.2, color="darkorange", lw=lw)
-            axes[i,j].grid()
-            axes[i,j].legend(loc="best")
-        return fig, axes
+        return plot_mclust(res, g_range=self.g_range, figsize=figsize, lw=lw)
     
     def plot_mclust_col(self, figsize=(15, 15), lw=2):
         self.plot_mclust(self.mclust_col_res, figsize, lw)
@@ -197,44 +186,15 @@ class M01F(object):
     def plot_pca_fa_cv(self, df=None, n_range=np.arange(2, 16), n_init=10,
                        cv=KFold(n_splits=5, shuffle=True),
                        figsize=(10,10)):
-        if df is None:
-            c = self.df_cor.values
-        pca = PCA(svd_solver='full')
-        fa = FactorAnalysis()
-        pca_scores, fa_scores = [], []
-        with tqdm(total=n_init, file=sys.stdout) as pbar:
-            for _ in range(n_init):
-                if df is None:
-                    r_calced = self._create_mat_selected_cor(c, self.X_df)
-                else:
-                    r_calced = df.values
-                for n in n_range:
-                    pca.n_components = n
-                    fa.n_components = n
-                    pca_scores.append(np.mean(cross_val_score(pca, r_calced, cv=cv)))
-                    fa_scores.append(np.mean(cross_val_score(fa, r_calced, cv=cv)))
-                pbar.update(1)
-        
-        pca_scores = np.array(pca_scores).reshape((n_init, -1))
-        fa_scores = np.array(fa_scores).reshape((n_init, -1))
-        self.cv_pca_scores = pca_scores
-        self.cv_fa_scores = fa_scores
-        
-        '''plot'''
-        plt.figure(figsize=figsize)
-        pca_m = pca_scores.mean(axis=0)
-        pca_std = np.std(pca_scores, axis=0)
-        plt.plot(n_range, pca_m, '-o', label='PCA')
-        if n_init != 1:
-            plt.fill_between(n_range, pca_m-pca_std, pca_m+pca_std, alpha=0.2, color="darkorange")
-        
-        fa_m = fa_scores.mean(axis=0)
-        fa_std = np.std(fa_scores, axis=0)
-        plt.plot(n_range, fa_m, '-o', label='FA')
-        if n_init != 1:
-            plt.fill_between(n_range, fa_m-fa_std, fa_m+fa_std, alpha=0.2, color="darkorange")
-        plt.grid()
-        plt.legend()
+        if df is not None:
+            x_mat = df.values
+            n_obs = df.shape[0]
+        else:
+            x_mat = self.df_cor.values
+            n_obs = self.X_df.shape[0]
+        self.cv_pca_scores, self.cv_fa_scores = plot_pca_fa_cv(x_mat, n_obs,
+                                                               n_range=n_range, n_init=n_init, cv=cv,
+                                                               figsize=figsize)
     
     def _create_mat_selected_cor(self, selected_cor, df):
         r_calced = create_mat_from_cor(selected_cor, df.shape[0])
@@ -264,6 +224,105 @@ class M01F(object):
     gamma = property(get_gamma)
 
 
+
+class M01F_li(object):
+    
+    def __init__(self, X_df):
+        self.X_df = X_df
+        self.find_ncomponents(type='pca', n_iter=10, figsize=(10,10))
+    
+    def find_ncomponents(self, type='pca', n_iter=10, figsize=(10,10)):
+        w, res = find_ncomponents_pca(self.X_df.values, n_iter=n_iter, figsize=figsize)
+        return w, res
+    
+    def set_n_sig(self, n_sig=3):
+        self.n_sig = n_sig
+        self.calc_cor_nonoise()
+        self.calc_loadings_nonoise()
+        self.calc_X_df_nonoise()
+        self.calc_row_fet()
+    
+    def calc_cor_nonoise(self):
+        c = self.X_df.corr()
+        m = calc_cor_nonoise(c, n_sig=self.n_sig)
+        self.cor_nonoise = m
+        return m
+    
+    def calc_loadings_nonoise(self):
+        v = calc_loadings_nonoise(self.cor_nonoise)
+        self.loadings_nonoise = v
+        return v
+    
+    def calc_X_df_nonoise(self):
+        x_sc2 = calc_mat_nonoise(self.X_df.values, self.n_sig)
+        self.X_df_nonoise = pd.DataFrame(x_sc2, index=self.X_df.index, columns=self.X_df.columns)
+        return self.X_df_nonoise
+    
+    def calc_row_fet(self):
+        ll = []
+        for ii in range(self.n_sig):
+            tmp = np.corrcoef(self.X_df_nonoise.values, self.loadings_nonoise.T[ii,:])[:self.X_df.shape[0], self.X_df.shape[0]:]
+            ll.append(tmp)
+        x_sc3 = np.concatenate(ll, axis=1)
+        self.row_fet = x_sc3
+        return x_sc3
+    
+    def mclust_col(self, n_init=N_INIT, g_range=G_RANGE, cov_type_list=cov_type_list,
+               gm=mixture.GaussianMixture(init_params='kmeans')):
+        self.mclust_col_res = mclust(self.loadings_nonoise[:,:self.n_sig], n_init=n_init, g_range=g_range,
+                                          cov_type_list=cov_type_list, gm=gm)
+        return self.mclust_col_res
+    
+    def mclust_row(self, n_init=N_INIT, g_range=G_RANGE, cov_type_list=cov_type_list,
+               gm=mixture.GaussianMixture(init_params='kmeans')):
+        self.mclust_row_res = mclust(self.row_fet, n_init=n_init, g_range=g_range,
+                                          cov_type_list=cov_type_list, gm=gm)
+        return self.mclust_row_res
+
+
+
+
+
+
+
+
+
+
+def calc_mat_nonoise(mat, n_sig=3):
+    ss = StandardScaler()
+    ss.fit(mat)
+    x_sc = ss.transform(mat)
+    u, s, vh = np.linalg.svd(x_sc)
+    x_sc2 = u[:,:n_sig].dot(np.diag(s[:n_sig] * s.sum() / s[:n_sig].sum())).dot(vh[:n_sig])
+    return ss.inverse_transform(x_sc2)
+
+
+def calc_cor_nonoise(c, n_sig=3):
+    try:
+        w, v = np.linalg.eig(c)
+    except Exception as e:
+        print(e)
+        c1 = cor_smooth(c)
+        w, v = np.linalg.eig(c1)
+        print('"Matrix was not positive definite, smoothing was done"')
+    m = v[:,:n_sig].dot(np.diag(w[:n_sig])).dot(v[:,:n_sig].T)
+    d = np.sqrt(np.diag(m)).reshape((v.shape[0],1))
+    m = m / d / d.T
+    return m
+
+
+def calc_loadings_nonoise(c):
+    w, v = np.linalg.eigh(c)
+    idx = np.argsort(w)[::-1]
+    w, v = w[idx], v[:,idx]
+    return v
+
+
+def eig(cor_mat):
+    sdev2, loadings = np.linalg.eig(cor_mat)
+    idx = np.argsort(sdev2)[::-1]
+    sdev2, loadings = sdev2[idx], loadings[:,idx]
+    return sdev2, loadings
 
 
 def create_mat_from_cor(selected_cor, n_samples=100, state=None):
@@ -300,19 +359,83 @@ def mclust(df_target, n_init=3, g_range=G_RANGE,
     return res
 
 
-def find_ncomponents_pca(x, n_obs=None, n_iter=10,
-                         figsize=(10,10)):
-    n_features = x.shape[1]
+def plot_mclust(res, g_range=G_RANGE, figsize=(15, 15), lw=2):
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=figsize)
+    
+    for ii, k in enumerate(cov_type_list):
+        i, j = divmod(ii, 2)
+        m = res[k].mean(axis=1)
+        std = np.std(res[k], axis=1)
+        axes[i,j].plot(g_range, m, label=k, marker='o')
+        axes[i,j].fill_between(g_range, m - std,
+                         m + std, alpha=0.2, color="darkorange", lw=lw)
+        axes[i,j].grid()
+        axes[i,j].legend(loc="best")
+    return fig, axes
+
+
+def plot_pca_fa_cv(x_mat, n_obs=None, 
+                   n_range=np.arange(2, 16), n_init=10,
+                   cv=KFold(n_splits=5, shuffle=True),
+                   figsize=(10,10)):
+    isCorrelation, c, n_obs = check_cor(x_mat, n_obs)
+    
+    pca = PCA(svd_solver='full')
+    fa = FactorAnalysis()
+    pca_scores, fa_scores = [], []
+    with tqdm(total=n_init, file=sys.stdout) as pbar:
+        for _ in range(n_init):
+            if isCorrelation:
+                r_calced = create_mat_from_cor(c, n_obs, state=None)
+            else:
+                r_calced = x_mat
+            for n in n_range:
+                pca.n_components = n
+                fa.n_components = n
+                pca_scores.append(np.mean(cross_val_score(pca, r_calced, cv=cv)))
+                fa_scores.append(np.mean(cross_val_score(fa, r_calced, cv=cv)))
+            pbar.update(1)
+    
+    pca_scores = np.array(pca_scores).reshape((n_init, -1))
+    fa_scores = np.array(fa_scores).reshape((n_init, -1))
+    
+    '''plot'''
+    plt.figure(figsize=figsize)
+    pca_m = pca_scores.mean(axis=0)
+    pca_std = np.std(pca_scores, axis=0)
+    plt.plot(n_range, pca_m, '-o', label='PCA')
+    if n_init != 1:
+        plt.fill_between(n_range, pca_m-pca_std, pca_m+pca_std, alpha=0.2, color="darkorange")
+    
+    fa_m = fa_scores.mean(axis=0)
+    fa_std = np.std(fa_scores, axis=0)
+    plt.plot(n_range, fa_m, '-o', label='FA')
+    if n_init != 1:
+        plt.fill_between(n_range, fa_m-fa_std, fa_m+fa_std, alpha=0.2, color="darkorange")
+    plt.grid()
+    plt.legend()
+    return pca_scores, fa_scores
+
+
+def check_cor(x, n_obs=None):
     isCorrelation = False
     if x.shape[0] == x.shape[1] and int(np.diag(x).sum()) == x.shape[0]:
         isCorrelation = True
         c = x
         if n_obs is None:
             raise Exception('if x is correlation matrix, n_obs must be specified.')
-        x = create_mat_from_cor(c, n_samples=n_obs, state=None)
     else:
         c = np.corrcoef(x, rowvar=False)
         n_obs = x.shape[0]
+    return isCorrelation, c, n_obs
+
+
+def find_ncomponents_pca(x, n_obs=None, n_iter=10,
+                         figsize=(10,10)):
+    n_features = x.shape[1]
+    isCorrelation, c, n_obs = check_cor(x, n_obs)
+    if isCorrelation:
+        x = create_mat_from_cor(c, n_samples=n_obs, state=None)
     
     '''calc PCA eigen values'''
     w = get_eigval(x, n_features)
@@ -361,7 +484,109 @@ def get_eigval(mat, n_features):
     return w
 
 
+def cor_smooth(cor_mat, tol=10e-12):
+    w, v = np.linalg.eigh(cor_mat)
+    if w.min() < np.finfo(float).eps:
+        w[w < tol] = tol * 100
+        nvar = cor_mat.shape[0]
+        tot = w.sum()
+        w2 = w * nvar / tot
+        new_cor = v.dot(np.diag(w2)).dot(v.T)
+        d = np.sqrt(np.diag(new_cor)).reshape((nvar,1))
+        new_cor = new_cor / d / d.T
+        return new_cor
+    return cor_mat
 
+
+
+
+
+def create_random_map2(mat, n_std=2, kshape_start=12, noise=True):
+    kshape = np.arange(2, kshape_start+1)[::-1]
+    
+    sc = StandardScaler()
+    sc.fit(mat)
+    mat_sc = sc.transform(mat)
+    
+    pca = PCA(n_components=mat_sc.shape[1], svd_solver='full')
+    pca.fit(mat_sc)
+    
+    tmp = np.linspace(-n_std, n_std, kshape[0]).reshape(kshape[0], 1) * pca.components_[0] * np.sqrt(pca.explained_variance_)[0]
+    
+    for ii in range(1, mat_sc.shape[1]):
+        idx = np.arange(ii+2)
+        idx[0], idx[ii] = idx[ii], idx[0]
+        if len(kshape) <= ii:
+            ii_kshape = 2
+        else:
+            ii_kshape = kshape[ii]
+#        ii_kshape = kshape[ii]
+#        if ii_kshape < 3:
+#            ii_kshape = 3
+        tmp = np.transpose(np.tile(tmp, [ii_kshape]+[1]*tmp.ndim), idx) + \
+            np.linspace(-n_std, n_std, ii_kshape).reshape(ii_kshape, 1) * pca.components_[ii] * np.sqrt(pca.explained_variance_)[ii]
+    
+    init_map = tmp.reshape((-1, mat_sc.shape[1]))
+    init_map = sc.inverse_transform(init_map)
+    return init_map
+
+def create_random_map(mat):
+    ss = StandardScaler()
+    mat_sc = ss.fit_transform(mat)
+
+    u, s, vh = np.linalg.svd(mat_sc)
+    x1 = mat_sc.dot(vh)
+    n_samples = x1.shape[0]
+    ll = []
+    for ii in range(x1.shape[1]):
+        min_x = x1[:,ii].min()
+        max_x = x1[:,ii].max()
+        ll.append((max_x - min_x) * np.random.random_sample(n_samples) + min_x)
+    x2 = np.c_[ll].T.dot(vh.T)
+    mat_ret = ss.inverse_transform(x2)
+    return mat_ret
+
+def find_nclusters_gap(mat, kshape_start=12, n_range=np.arange(2, 31), nn=20,
+                       figsize=(20,8)):
+    #rand_map0 = create_random_map(mat, noise=False, kshape_start=kshape_start)
+    
+    def score(mat, cls):
+        cls_unique = np.unique(cls)
+        sc = 0.0
+        for icls in cls_unique:
+            mat_part = mat[cls == icls,:]
+            sc += pairwise.euclidean_distances(mat_part).sum()
+        return sc / mat.shape[0]**2
+    
+    scores = {'data': [], 'rand': []}
+    with tqdm(total=nn, file=sys.stdout) as pbar:
+        for jj in range(nn):
+            #idx = np.random.choice(np.arange(rand_map0.shape[0]), size=mat.shape[0], replace=False)
+            #rand_map = rand_map0[idx,:]
+            rand_map = create_random_map(mat)
+            for ii in n_range:
+                kmeans = KMeans(n_clusters=ii, n_init=1)
+                kmeans.fit(mat)
+                # scores['data'].append(-kmeans.score(mdl.fet_row_aggr))
+                cls = kmeans.predict(mat)
+                scores['data'].append(score(mat, cls))
+        
+                #kmeans = KMeans(n_clusters=ii, n_init=1)
+                #kmeans.fit(rand_map)
+                # scores['rand'].append(-kmeans.score(rand_map))
+                #scores['rand'].append(score(rand_map, kmeans.predict(rand_map)))
+                scores['rand'].append(score(rand_map, cls))
+            pbar.update(1)
+    
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+    axes[0].plot(n_range, np.array(scores['data']).reshape((nn, -1)).T.mean(axis=1), color='darkblue', marker='o', label='data')
+    axes[0].plot(n_range, np.array(scores['rand']).reshape((nn, -1)).T.mean(axis=1), color='darkorange', marker='o', label='rand')
+    axes[0].grid()
+    axes[0].legend()
+    
+    axes[1].plot(n_range, -np.array(scores['data']).reshape((nn, -1)).T.mean(axis=1)/np.array(scores['rand']).reshape((nn, -1)).T.mean(axis=1), marker='o')
+    axes[1].grid()
+    return scores
 
 
 
