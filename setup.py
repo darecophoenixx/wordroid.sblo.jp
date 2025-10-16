bb1c311bd9335350f1435726a45af535c8d22b51
darecophoenixx <tmdnro.phoenixx@gmail.com>
2025-10-17 00:26:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ setup(
     name='wordroid.sblo.jp',
     authoer='Norio Tamada',
     url='https://github.com/darecophoenixx/wordroid.sblo.jp',
-    version='0.00.01',
+    version='0.00.02',
     package_dir={
         'feature_eng': 'lib/feature_eng',
         'keras_ex.gkernel': 'lib/keras_ex/gkernel',
         'keras_ex.HumanisticML': 'lib/keras_ex/HumanisticML',
         'som': 'lib/som',
+        'egreedy': 'lib/egreedy'
         },
     packages=[
         'feature_eng',
         'keras_ex.gkernel',
         'keras_ex.HumanisticML',
         'som',
+        'egreedy',
         ],
     install_requires=['gensim',],
 )
